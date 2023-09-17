c42b285608d38d1e0080dd185fa0608a5f44af74
Asante-ntiro <asante.ntiro@gmail.com>
2023-09-17 21:17:57 +0300

    :zap:

--- to-whom-does-this-wallet-belong/src/main.rs
+++ to-whom-does-this-wallet-belong/src/main.rs
@@ -1,9 +1,13 @@
 use anyhow::Result;
 mod wallet;
 
+
+//const URL :&str = ""
 fn main() -> Result<()>{
     let keypair = wallet::create_keypair();
     println!("keypair: {:?}", keypair);
+
+//    let web3 = wallet::connect_to_web3(url)
 
     Ok(())
 }
--- to-whom-does-this-wallet-belong/src/wallet.rs
+++ to-whom-does-this-wallet-belong/src/wallet.rs
@@ -1,8 +1,15 @@
 use secp256k1::{PublicKey, SecretKey, rand::{rngs, SeedableRng}};
-use anyhow::Result;
+use anyhow::{Result, Ok};
+use web3::{*, transports::Http};
+
 
 pub fn create_keypair() -> Result<(SecretKey, PublicKey)> {
     let secp = secp256k1::Secp256k1::new();
     let mut rng = rngs::StdRng::seed_from_u64(6);
     Ok(secp.generate_keypair(&mut rng))
+}
+
+pub fn connect_to_web3(url: &str) -> Result<Web3<Http>> {
+    let transport = web3::transports::Http::new(url)?;
+    Ok(web3::Web3::new(transport))
 }
