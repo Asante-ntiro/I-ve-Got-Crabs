68d612c64e620aa116b801aac844d8285762e10a
Asante-ntiro <asante.ntiro@gmail.com>
2024-07-29 17:46:17 +0300

    :zap: added node and device registry :zap:

--- trie-stuff/iot-trie/src/lib.rs
+++ trie-stuff/iot-trie/src/lib.rs
@@ -1,14 +1,34 @@
+use std::{collections::HashMap, cell::RefCell, rc::Rc};
+
+type Link = Option<Rc<RefCell<Node>>>;
+
 #[derive(Clone, Debug)]
 pub struct IoTDevice {
     pub numerical_id: u64,
     pub address: String,
 }
 
+pub struct Node {
+    pub key: char,
+    next: HashMap<char, Link>,
+    pub value: Option<IoTDevice>,
+}
 
 
+pub struct DeviceRegistry {
+    pub length: u64,
+    root: HashMap<char, Link>,
+}
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+impl DeviceRegistry{
+    pub fn add(&mut self, device:IoTDevice) {
+        let p = device.address.clone();
+        let mut path = p.chars();
+        if let Some(start) = path.next() {
+            todo!()
+        }
+
+    }
 }
 
 #[cfg(test)]
