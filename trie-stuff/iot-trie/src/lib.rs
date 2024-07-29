0f439b9c42e6bab70bd71a176d119f3203ea2ec5
Asante-ntiro <asante.ntiro@gmail.com>
2024-07-29 17:20:06 +0300

    :zap: tries are cool

---
+++ trie-stuff/iot-trie/src/lib.rs
@@ -0,0 +1,23 @@
+#[derive(Clone, Debug)]
+pub struct IoTDevice {
+    pub numerical_id: u64,
+    pub address: String,
+}
+
+
+
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
