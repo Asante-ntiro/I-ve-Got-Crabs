028f9785280690ccd91515a41ade24a099331491
Asante-ntiro <asante.ntiro@gmail.com>
2024-07-30 23:56:19 +0300

    :zap:

--- trie-stuff/regular-trie/src/main.rs
+++ trie-stuff/regular-trie/src/main.rs
@@ -18,6 +18,22 @@ impl Trie {
         Trie::default()
     }
 
+    pub fn insert(&mut self, word: &str) {
+        let mut current_node = &mut self.root;
+
+        for c in word.chars() {
+            current_node = current_node.children
+                .entry(c)
+                .or_default();
+        }
+
+        current_node.end_of_word = true;
+    }
+
+    pub fn search(&self, word: &str) -> bool {
+        let mut current_node = &self.root;
+    }
+
 }
 
 fn main() {
